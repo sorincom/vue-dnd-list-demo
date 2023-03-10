1757e2bb4ec72a51ff184a9c5ea169469cb7ae6a
Sorin Comanescu <comanescu.sorin@gmail.com>
2023-03-10 16:48:30 +0200

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import SimpleView from '../views/SimpleView.vue'
 import TwoListsView from '../views/TwoListsView.vue'
 import CustomDragSourceView from '../views/CustomDragSourceView.vue'
 import CustomAcceptView from '../views/CustomAcceptView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
